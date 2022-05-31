f440c98480355628d3a506561e59a1c5294d8247
Thomaz Junior <p.thomaz.junior@gmail.com>
2022-05-31 09:19:55 +0100

    Netflix clone project initializade

---
+++ components/Banner.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { Movie } from '../typings';
+
+interface Props {
+    netflixOriginals: Movie[];
+}
+
+function Banner({  netflixOriginals  }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null);
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
+  console.log(movie);
+
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>Lets build Netflix Clone with Nextjs, Taiwlandcss and Typescript</div>
+  )
+}
+
+export default Login
